2958b78a0d3c29714c47ca84f57066a1feca11e9
Roger Earl <rogerearl@me.com>
2023-01-10 20:24:44 -0800

    Jazzed up form page

--- controllers/places.js
+++ controllers/places.js
@@ -13,14 +13,42 @@ router.get('/', (req, res) => {
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
         pic: '/images/cafe.jpg'
-      }]
-      
+      }]      
     res.render('places/index', { places })
   })
 
-  router.get('/new', (req, res) => {
-    res.render('places/new')
-  })
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places stub')
+})
+
+router.get('/new', (req, res) => {
+  res.render('places/new')
+})
+
+router.get('/:id', (req, res) => {
+  res.send('GET /places/:id stub')
+})
+
+router.put('/:id', (req, res) => {
+  res.send('PUT /places/:id stub')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('DELETE /places/:id stub')
+})
+
+router.get('/:id/edit', (req, res) => {
+  res.send('GET /places/:id/edit stub')
+})
+
+router.post('/:id/rant', (req, res) => {
+  res.send('GET /places/:id/rant stub')
+})
+
+router.delete('/:id/rant/:rantId', (req, res) => {
+  res.send('GET /places/:id/rant/:rantId stub')
+})
   
   
 module.exports = router
