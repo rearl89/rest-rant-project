770dd2f95198ede3e38fb64d65f5cb9efec3ac27
Roger Earl <rogerearl@me.com>
2023-01-21 10:55:21 -0800

    Created and used a helper method

--- views/error404.jsx
+++ views/error404.jsx
@@ -8,7 +8,7 @@ function error404 () {
                 <h1>404: PAGE NOT FOUND</h1>
                 <p>Oops, sorry, we can't find this page!</p>
                 <div>
-                    <img src="http://placekitten.com/300/300" alt="cat" />
+                    <img src="/images/puppy.jpg" alt="puppy" />
                 </div>
             </main>
         </Def>
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,18 +5,19 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{ data.place.name }</h1>
-            <img src={data.place.pic} />
+            <h1>{data.place.name}</h1>
+            <img src={data.place.pic} alt={data.place.name} />
+            <h3>Located in {data.place.city}, {data.place.state}</h3>
             <h2>Rating</h2>
             <h4>currently unrated</h4>
             <h2>Description</h2>
-            <h4>Located in {data.place.city}, {data.place.state}, and serving {data.place.cuisines}.</h4>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
             <h2>Comments</h2>
             <h4>no comments yet</h4>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
-            </a>     
-
+            </a>
             <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">
                     Delete
