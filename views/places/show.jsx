e7b7930024de56ee4b765d97838aab96a3ae986b
Roger Earl <rogerearl@me.com>
2023-01-23 19:59:26 -0800

    Populate the comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,7 +1,26 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
+function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if(data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! 😡' : 'Rave! 😍'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
           <main>
@@ -14,7 +33,7 @@ function show () {
             <h3>{data.place.showEstablished()}</h3>
             <h4>Serving {data.place.cuisines}</h4>
             <h2>Comments</h2>
-            <h4>no comments yet</h4>
+            {comments}
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
                 Edit
             </a>
